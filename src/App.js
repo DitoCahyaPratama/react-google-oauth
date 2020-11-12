89eeefafcb7b571627ba7768d101a714bcad8e8e
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2020-11-12 23:56:36 +0700

    Push Master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {GoogleLogin} from 'react-google-login'
+
+const responseGoogle = response => {
+  console.log(response)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin clientId="your-client-id" onSuccess={responseGoogle} onFailure={responseGoogle} />      
     </div>
   );
 }
